794eaf8c2db0a985cd7783716367239a10aee908
WeiJiangZHCN <2627218531@qq.com>
2024-04-16 09:04:19 +0800

    提升一下兼容性

--- dist/counter.js
+++ dist/counter.js
@@ -1,114 +1,59 @@
-let s = 0, r = [];
-function d() {
-  return document.querySelectorAll("[topic]");
-}
-function m(e) {
-  return document.querySelector(`[topic='${e}']`).children[1].childElementCount;
-}
-s = d().length;
-for (let e = 1; e <= s; e++)
-  r.push(m(e));
-console.log(`问题数量为${s}`);
-console.log(`答案数量列表为${r}`);
-let u = document.createElement("div");
-u.style.cssText = "position: absolute; top: 0; right: 0;";
-document.body.appendChild(u);
-function _(e, t, n) {
-  let l = document.createElement("input");
-  l.value = t, l.id = n, e.appendChild(l);
-}
-function p(e, t, n) {
-  let l = document.createElement("label");
-  l.innerText = t, l.htmlFor = n, e.appendChild(l);
-}
-function h() {
-  for (let [e, t] of r.entries()) {
-    let n = document.createElement("div"), l = e + 1;
-    p(n, "问题 " + l, "mq" + e), _(n, t, "mq" + e), u.appendChild(n);
+let r = [], f = document.querySelectorAll("[topic]");
+if (localStorage.getItem("questions_info"))
+  r = JSON.parse(localStorage.getItem("questions_info"));
+else
+  for (let [t, l] of f.entries()) {
+    let e = {
+      id: `${t + 1}`,
+      //问题序号
+      type: l.getAttribute("type"),
+      //问题类型
+      number: void 0,
+      //问题选项数量
+      maxvalue: l.getAttribute("maxvalue"),
+      value: l
+    };
+    (e.type = "5") ? e.number = l.querySelectorAll("li").length : e.number = l.children[1].childElementCount, r.push(e);
   }
+function s(t, l) {
+  document.querySelector(`[for='q${t}_${l}']`).click();
 }
-h();
-let c = document.createElement("button");
-c.innerText = "提交";
-c.addEventListener("click", function() {
-  v([1, 3, 4, 3, 3, 2, 2, 3, 5, 4, 3]), g(u);
-});
-u.appendChild(c);
-function g(e) {
-  let t = e.querySelectorAll("[id^='mq']");
-  for (let [n, l] of t.entries()) {
-    let o = l.value.split(",").map((i) => parseFloat(i));
-    r[n] = o;
-  }
-  console.log(r);
+function o(t) {
+  return Math.ceil(Math.random() * t);
 }
-function v(e) {
-  for (let [t, n] of e.entries()) {
-    let l = document.querySelector("#mq" + t);
-    l.value = b(n).toString();
-  }
-}
-function b(e) {
-  let t = [];
-  if (e === 1)
-    return [1];
-  for (let n = 0; n < e; n++)
-    if (n === 0) {
-      let l = Math.ceil(Math.random() * 100) / 100;
-      t.push(l);
-    } else if (n < e - 1) {
-      let l = 0;
-      for (; !(l > 100); ) {
-        l++, console.log("1");
-        let o = Math.ceil(Math.random() * 100) / 100;
-        if (o < 1 - f(t)) {
-          t.push(o);
-          break;
+function m(t) {
+  for (let [l, e] of r.entries()) {
+    if (e.type === "4") {
+      let i = o(e.maxvalue);
+      console.log(i);
+      let c = {};
+      for (let a = 0; a < i; a++)
+        for (; ; ) {
+          let u = o(e.number);
+          if (c[u] === void 0) {
+            c[u] = 1, s(l + 1, u);
+            break;
+          }
         }
-      }
-    } else
-      n === e - 1 && t.push(Math.ceil(100 - f(t) * 100) / 100);
-  return t;
-}
-function f(e) {
-  let t = 0;
-  for (let [n, l] of e.entries())
-    t += l;
-  return t;
-}
-function w(e, t) {
-  document.querySelector(`[for='q${e}_${t}']`).click();
-}
-function q(e) {
-  var n;
-  let t = (n = localStorage.getItem("answer_count_lists")) == null ? void 0 : n.split(",");
-  if (t)
-    y(e, t);
-  else {
-    let l = [];
-    for (let [o, i] of r.entries()) {
-      l.push(Array(i).fill(0));
-      let a = S(i);
-      w(o + 1, a), l[o][a] += 1;
+      continue;
     }
-    localStorage.setItem("answer_count_lists", l);
+    if (e.type === "5") {
+      let i = o(100);
+      i <= 8 ? e.value.querySelectorAll("li")[0].click() : i <= 12 ? e.value.querySelectorAll("li")[1].click() : i <= 20 ? e.value.querySelectorAll("li")[2].click() : i <= 40 ? e.value.querySelectorAll("li")[3].click() : e.value.querySelectorAll("li")[4].click();
+      continue;
+    }
+    let n = o(e.number);
+    s(l + 1, n);
   }
 }
-function y(e, t) {
-  for (let [n, l] of t.entries())
-    for (let [o, i] of l.entries())
-      if (E(n, list) < e[n][o]) {
-        i(n, o);
-        break;
-      }
+setTimeout(function() {
+  document.getElementById("ctlNext").click();
+}, 2e4);
+setTimeout(m, 2e3);
+function d() {
+  for (var t = document.cookie.split(";"), l = 0; l < t.length; l++) {
+    var e = t[l], n = e.indexOf("="), i = n > -1 ? e.substr(0, n) : e;
+    document.cookie = i + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT";
+  }
 }
-function S(e) {
-  return Math.ceil(Math.random() * e);
-}
-function E(e, t) {
-  let n = 0;
-  for (let l of t)
-    n += l;
-  return t[e] / n;
-}
-q();
+d();
